0e01604c1e536f13d8764959efe3cab34324679a
Carlos Matheu Armengol <info@plastikaweb.com>
2016-09-16 12:35:56 +0200

    simplify add event and notify registering methods

--- events.js
+++ events.js
@@ -5,20 +5,17 @@ var EventTracker = function(name) {
 };
 EventTracker.prototype.on = function(eventName, callback) {
     if (!this.events[eventName]) {
-        this.events[eventName] = [callback];
-    } else {
-        this.events[eventName].push(callback);
+        this.events[eventName] = [];
     }
+    this.events[eventName].push(callback);
 };
 EventTracker.prototype.notify = function(parentEventTracker, eventName) {
     if (!this.notifies[eventName]) {
-        this.notifies[eventName] = [parentEventTracker];
-    } else {
-        this.notifies[eventName].push(parentEventTracker);
+        this.notifies[eventName] = [];
     }
+    this.notifies[eventName].push(parentEventTracker);
 };
 EventTracker.prototype.trigger = function(eventName, param) {
-
     try {
         if( !this.events[eventName] && !this.notifies[eventName]) {
             throw new Error('The event ' + eventName + ' does not appear as valid for the ' + this.name + ' tracker');
@@ -30,7 +27,6 @@ EventTracker.prototype.trigger = function(eventName, param) {
                 callbacks[i].call(this, param);
             }
         }
-
         // notifies
         if (this.notifies[eventName]) {
             var notifies = this.notifies[eventName];
